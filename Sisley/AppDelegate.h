57f3ec8a3561087b307c979e63ad408b77a975d1
Adrien Lavisiera <adrienl@outlook.com>
2015-11-03 11:38:58 +0100

    Initial Commit

---
+++ Sisley/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sisley
+//
+//  Created by ZIIW on 03/11/2015.
+//  Copyright © 2015 Ziiw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sisley/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sisley
+//
+//  Created by ZIIW on 03/11/2015.
+//  Copyright © 2015 Ziiw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
