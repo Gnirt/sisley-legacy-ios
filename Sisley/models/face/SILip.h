b33ef8ba7f08b0a1b9e9b777dbccf0c4978bb187
Adrien Lavisiera <adrienl@outlook.com>
2015-11-11 15:04:07 +0100

    Add file

---
+++ Sisley/models/face/SILip.h
@@ -0,0 +1,20 @@
+//
+//  SILip.h
+//  Sisley
+//
+//  Created by ZIIW on 10/11/2015.
+//  Copyright © 2015 Ziiw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+#import "SIFaceItem.h"
+
+@interface SILip : SIFaceItem
+
+@property (nonatomic) CGPoint cornerLeft;
+@property (nonatomic) CGPoint lineMiddle;
+@property (nonatomic) CGPoint cornerRight;
+
+@end
