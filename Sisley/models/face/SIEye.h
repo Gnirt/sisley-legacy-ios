300ba7f0ef79b8a058900b54f594196d0fbf5efd
Adrien Lavisiera <adrienl@outlook.com>
2015-11-11 15:03:29 +0100

    Création des class pour ranger els données

---
+++ Sisley/models/face/SIEye.h
@@ -0,0 +1,23 @@
+//
+//  SIEye.h
+//  Sisley
+//
+//  Created by ZIIW on 10/11/2015.
+//  Copyright © 2015 Ziiw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "SIFaceItem.h"
+
+@interface SIEye : SIFaceItem
+
+@property (nonatomic) CGPoint cornerLeft;
+@property (nonatomic) CGPoint center;
+@property (nonatomic) CGPoint cornerRight;
+@property (nonatomic) CGPoint browLeft;
+@property (nonatomic) CGPoint browMiddle;
+@property (nonatomic) CGPoint browRight;
+
+@end
